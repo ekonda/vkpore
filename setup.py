64a2f4699cd7958d0c07f4b2c9dd2903ad51b170
Michael Krukov <krukov.michael@ya.ru>
2020-05-23 00:49:38 +0300

    Bumped version

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open('README.md', "r") as f:
 
 setup(
     name='vkpore',
-    version='0.1.0',
+    version='0.1.1',
     description='Asynchronous library for organizing interactions with Vkontakte api',
     long_description=long_description,
     long_description_content_type='text/markdown',
